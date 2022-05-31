e6c69dc2eb77c6c71a965806cb83e01aad9b8c23
Aadhaar-debug <75715900+Aadhaar-debug@users.noreply.github.com>
2022-06-01 00:42:20 +0530

    Add files via upload

---
+++ Friendfunction.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class number
+{
+    private:
+        int  a = 50;
+        int  b = 30;
+    public:
+        friend int try_access_private_data(number b);
+        friend int access_private_data(number c);
+};
+int try_access_private_data(number b)
+{
+    cout<< b.a + b.b <<endl;
+}
+int access_private_data(number c)
+{
+    cout<<c.a+c.b<<endl;
+}
+
+
+int main()
+{
+    number z;
+    try_access_private_data(z);
+    access_private_data(z);
+    return 0;
+}
